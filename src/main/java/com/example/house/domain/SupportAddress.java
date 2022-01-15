ee80fc7b1245f25d8be3abf6fc100b2d80d9edaa
peter <1366565150@qq.com>
2022-01-15 19:31:30 +0800

    1

--- src/main/java/com/example/house/domain/SupportAddress.java
+++ src/main/java/com/example/house/domain/SupportAddress.java
@@ -9,7 +9,25 @@ public class SupportAddress {
     private String enName;
     private String cnName;
     private String level;
-    private Double baiduMapLng;
-    private Double baiduMapLat;
+    private Double baiduMapLongitude;
+    private Double baiduMapLatitude;
+    public enum Level{
+        CITY("city"),
+        REGION("region");
+
+        private String value;
+
+        Level(String value) {this.value =value;}
+
+        public String getValue(){return value;}
+
+        public static Level of(String value) {
+            for (Level level : Level.values()) {
+                if (level.getValue().equals(value)) {
+                    return level;
+                }
+            } throw new IllegalArgumentException();
+        }
+            }
 
 }
